0f3c534e2de034db4c44cbe971162e5d46d85f05
Eric Muradyan <ermanfantasy@gmail.com>
2024-03-02 17:16:41 +0400

    Update: logo as link to home page

--- apps/frontend/src/app/components/layout/header/index.tsx
+++ apps/frontend/src/app/components/layout/header/index.tsx
@@ -5,9 +5,11 @@ export default function Header() {
   return (
     <AppBar component="header" position="static" sx={{ p: { xs: 4, xl: 2 } }}>
       <Box sx={{ display: "flex", justifyContent: "space-between", p: 0 }}>
-        <Typography component="h1" sx={{ fontSize: { xs: 20, xl: 30 } }}>
-          Logo
-        </Typography>
+        <NavLink to="/" withoutEffects>
+          <Typography component="h1" sx={{ fontSize: { xs: 20, xl: 30 } }}>
+            Logo
+          </Typography>
+        </NavLink>
         <NavLink to="/ads">Ads</NavLink>
       </Box>
     </AppBar>
