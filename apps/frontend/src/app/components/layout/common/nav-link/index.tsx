6cc2eaea26ff1e896836f35b860738ea33ffb0fd
Eric Muradyan <ermanfantasy@gmail.com>
2024-03-02 17:16:06 +0400

    Update: NavLink props

--- apps/frontend/src/app/components/layout/common/nav-link/index.tsx
+++ apps/frontend/src/app/components/layout/common/nav-link/index.tsx
@@ -1,15 +1,18 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-props-no-spreading */
 import { Box } from "@mui/material";
 import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 interface LinkProps {
   to: string;
+  withoutEffects?: boolean;
 }
 
-export default function NavLink({ to, children }: PropsWithChildren<LinkProps>) {
+export default function NavLink({ to, withoutEffects = false, children }: PropsWithChildren<LinkProps>) {
   return (
-    <Box>
-      <Link className="nav-link" href={to}>
+    <Box sx={{ p: 0.5 }}>
+      <Link {...(!withoutEffects && { className: "nav-link" })} href={to}>
         {children}
       </Link>
     </Box>
