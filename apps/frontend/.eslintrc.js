5d5707757d2c18ae2eb4ece416b742f0bb5c2086
Eric Muradyan <ermanfantasy@gmail.com>
2024-03-02 16:44:47 +0400

    Update: eslint config for tsx files and default exports

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,44 +1,40 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  'extends': [
-    'plugin:@nx/react-typescript',
-    'next',
-    'next/core-web-vitals',
-    'airbnb',
-    'airbnb/hooks',
-    '../../.eslintrc.json'
-  ],
-  'parserOptions': {
-    project: path.resolve(__dirname, '../../apps/frontend/tsconfig.json'),
+  extends: ["plugin:@nx/react-typescript", "next", "next/core-web-vitals", "airbnb", "airbnb/hooks", "../../.eslintrc.json"],
+  parserOptions: {
+    project: path.resolve(__dirname, "../../apps/frontend/tsconfig.json")
   },
-  'ignorePatterns': ['!**/*', '.next/**/*', 'jest.config.ts', '.eslintrc.js', 'next-env.d.ts'],
-  'overrides': [
+  ignorePatterns: ["!**/*", ".next/**/*", "jest.config.ts", ".eslintrc.js", "next-env.d.ts"],
+  overrides: [
     {
-      'files': ['*.ts', '*.tsx', '*.js', '*.jsx'],
-      'rules': {
-        '@next/next/no-html-link-for-pages': ['error', 'apps/frontend/pages'],
-        'react/function-component-definition': 'off',
-        'eslint-plugin-import/no-extraneous-dependencies': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'no-console': 'error',
-        'react/prop-types': 'off',
-        "spaced-comment": ["error", "always", { "exceptions": ["*", "@ts-check"] }]
+      files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
+      rules: {
+        "@next/next/no-html-link-for-pages": ["error", "apps/frontend/pages"],
+        "react/function-component-definition": "off",
+        "eslint-plugin-import/no-extraneous-dependencies": "off",
+        "import/no-extraneous-dependencies": "off",
+        "no-console": "error",
+        "react/prop-types": "off",
+        "spaced-comment": ["error", "always", { exceptions: ["*", "@ts-check"] }],
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".tsx"] }],
+        "import/prefer-default-export": "off",
+        "react/react-in-jsx-scope": "off"
       }
     },
     {
-      'files': ['*.ts', '*.tsx'],
-      'rules': {}
+      files: ["*.ts", "*.tsx"],
+      rules: {}
     },
     {
-      'files': ['*.js', '*.jsx'],
-      'rules': {}
+      files: ["*.js", "*.jsx"],
+      rules: {}
     },
     {
-      'files': ['*.spec.ts', '*.spec.tsx', '*.spec.js', '*.spec.jsx'],
-      'env': {
-        'jest': true
+      files: ["*.spec.ts", "*.spec.tsx", "*.spec.js", "*.spec.jsx"],
+      env: {
+        jest: true
       }
     }
   ]
-}
+};
